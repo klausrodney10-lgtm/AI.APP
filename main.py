80575ee65b9b89145dd343fbde4637781aedaa1c
klausrodney10-lgtm <klaus.rodney10@gmail.com>
2026-07-30 10:56:59 -0400

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.tilte("My name is Klaus")
 
+st.write("Anything you like a lot")
+st.header("My first app")
+st.subheader("My second app")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+count = 0
 
+if st.button("Click me"):
+    count += 1
+st.write("count is", count)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
